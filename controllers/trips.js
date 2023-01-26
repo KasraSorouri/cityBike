cd3402b881e0c78cc321bfabdf729e78e827c03e
Kasra Sorouri <k.sorouri@gmail.com>
2023-01-26 22:22:52 +0200

    Add Sort for Trips

--- controllers/trips.js
+++ controllers/trips.js
@@ -1,14 +1,12 @@
 const tripRouter = require('express').Router()
 const Trip = require('../models/trip.js')
-const dataAnalys = require('../utils/dataAnalys')
 
 tripRouter.get('/:page/:rowsPerPage',  async (request, response) => {
   const filter = request.query
-  const sortParameter = request.body.sort
   const page = request.params.page
   const rows = request.params.rowsPerPage
   let searchParameter = {}
-  console.log('is any patrams >', Object.keys(filter).length )
+  let sortParameter= {}
 
   if ( Object.keys(filter).length > 0 ) {
     filter.originStation !== 'null' ? searchParameter.departureStationId =`${filter.originStation}` : null
@@ -23,29 +21,22 @@ tripRouter.get('/:page/:rowsPerPage',  async (request, response) => {
     filter.distanceFrom === 'null' && filter.distanceTo !== 'null' ? searchParameter.distance={ $lte: filter.distanceTo*1000 } : null
     filter.distanceFrom !== 'null' && filter.distanceTo !== 'null' ? searchParameter.distance={ $gte: filter.distanceFrom*1000 , $lte: filter.distanceTo*1000} : null
   }
+ 
+  sortParameter[request.query.orderBy] = request.query.order === 'desc' ? -1 : 1
 
-    console.log('pagination * page ->', request.params.page, '   rows ->', rows)
-  console.log('Search query ->', request.query)
-  //console.log('Search Items ->', filter)
+  //console.log('pagination * page ->', request.params.page, '   rows ->', rows)
+  //console.log('Search query ->', request.query)
   console.log('Search params ->',searchParameter)
   console.log('Sort params ->', sortParameter)
  
   try {
     const totalTrips = await Trip.find({...searchParameter}).countDocuments()
-    const trips = await Trip.find({...searchParameter}).sort({ ...sortParameter }).skip(page*rows).limit(rows)
+    const trips = await Trip.find({...searchParameter}).sort(sortParameter).skip(page*rows).limit(rows)
     const body = { trips, totalTrips }
     response.json(body)
   } catch (e) {
     console.log(e.message);
   }
-})
-
-tripRouter.get('/:sid', async (request, response) => {
-
-  const stationId = request.params.sid
-//  console.log('Search params ->', stationId);
-  const body  =  await dataAnalys.statistic(stationId)
-  response.status(200).json(body)
 })
 
 module.exports = tripRouter
